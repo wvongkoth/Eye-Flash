efaec4f068542bc9b4caf4525e27c6b5f0a522bc
Will Vongkoth <avongkoth92@u.northwestern.edu>
2018-02-22 16:09:13 -0600

    was able to render vanilla homepage

---
+++ final-project/src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Cards from "../../components/Cards";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Navbar from "../../components/Navbar";
+
+const Home = () =>
+	<div>
+		<Navbar />
+		<Cards />
+		<Header />
+		<Footer />
+	</div>
+
+
+export default Home;
