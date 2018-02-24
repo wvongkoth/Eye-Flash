da88664b7e4f8583c034d2b6f1157f57b3876985
Will Vongkoth <avongkoth92@u.northwestern.edu>
2018-02-24 11:32:23 -0600

    set up react router

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MainNav from "./components/MainNav";
-import Card from "./components/Card";
+import Home from "./pages/Home";
 
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <MainNav />
-        <Card />
-      </div>
-    );
-  }
-}
+const App = () =>
+  <Router>
+    <div>
+      <MainNav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </div>
+  </Router>;
 
 export default App;
