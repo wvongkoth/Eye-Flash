3606091dde3db85592b3fbcafc4084c48ac3e679
Will Vongkoth <avongkoth92@u.northwestern.edu>
2018-02-28 16:22:19 -0600

    created api routes for get all decks to render on home page through minicards component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -11,11 +11,14 @@ const App = () =>
   <Router>
     <div>
       <MainNav />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signup" component={UserSignUp} />
-      </Switch>
+      	<div className="container">
+	      <Switch>
+	        <Route exact path="/" component={Home} />
+	        <Route exact path="/signup" component={UserSignUp} />
+	      </Switch>
+	     </div>
     </div>
   </Router>;
 
 export default App;
+
--- my-react-app/src/pages/Home/Home.js
+++ my-react-app/src/pages/Home/Home.js
@@ -1,12 +1,50 @@
 import React, { Component } from "react";
 import JumboCard from "../../components/JumboCard";
 import Form from "../../components/Form";
+import MiniCard from "../../components/MiniCard";
 
-const Home = () =>
-	<div>
-		<Form />
-		<JumboCard />
-	</div>
+class Home extends Component{
+	constructor(props) {
+		super(props);
+			this.state = {
+				error: null,
+				isLoaded: false,
+				decks: []
+			};
+	}
+
+  	componentDidMount() {
+	    fetch("/api/getAllDecks")
+	    	.then(res => res.json())
+	    	.then(
+	        (result) => {
+		        this.setState({
+		            isLoaded: true,
+		            decks: result.decks
+				});
+	   		},
+	   		(error) => {
+	   			this.setState({
+	   				isLoaded: true,
+	   				error
+	   			});
+	   		}
+	   	)
+	}	
+
+	render(){
+		return(
+			<div>
+				{this.state.decks.map(deck => (
+					<MiniCard />
+
+					))}
+			</div>
+		)
+	}
+
+}
+
 
 
 export default Home;
