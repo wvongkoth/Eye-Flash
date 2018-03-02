300b7e38d1e7cf853101b9f8fed4425425c1999b
Will Vongkoth <avongkoth92@u.northwestern.edu>
2018-03-01 21:22:17 -0600

    blaeh

--- my-react-app/src/pages/Home/Home.js
+++ my-react-app/src/pages/Home/Home.js
@@ -2,73 +2,89 @@ import React, { Component } from "react";
 import JumboCard from "../../components/JumboCard";
 import MiniCard from "../../components/MiniCard";
 import Form from "../../components/Form";
+import array from "../../array.json";
+import WordJC from "../../components/WordJC";
 
-// class Home extends Component{
-// 	constructor(props) {
-// 		super(props);
-// 			this.state = {
-// 				error: null,
-// 				isLoaded: false,
-// 				decks: []
-// 			};
-// 	}
+class Home extends Component{
+	constructor(props) {
+		super(props);
+			this.state = {
+				error: null,
+				isLoaded: false,
+				decks: []
+			};
+	}
 
-//   	componentDidMount() {
-// 	    fetch("/api/getAllDecks")
-// 	    	.then(res => res.json())
-// 	    	.then(
-// 	        (result) => {
-// 		        this.setState({
-// 		            isLoaded: true,
-// 		            decks: result.decks
-// 				});
-// 	   		},
-// 	   		(error) => {
-// 	   			this.setState({
-// 	   				isLoaded: true,
-// 	   				error
-// 	   			});
-// 	   		}
-// 	   	)
-// 	}	
+  	componentDidMount() {
+	    fetch("/api/getAllDecks")
+	    	.then(res => res.json())
+	    	.then(
+	        (result) => {
+		        this.setState({
+		            isLoaded: true,
+		            decks: result.decks
+				});
+	   		},
+	   		(error) => {
+	   			this.setState({
+	   				isLoaded: true,
+	   				error
+	   			});
+	   		}
+	   	)
+	}	
 
 // 	render(){
-// 		return(
-// 			<div className = 'container'>
-// 				{this.state.decks.map(deck => (
-// 					<MiniCard />
+		// return(
+		// 	<div className = 'container'>
+		// 		{this.state.decks.map(decks => (
+		// 			<MiniCard 
+						
+		// 			/>
 
-// 					))}
-// 			</div>
-// 		)
+		// 			))}
+		// 	</div>
+		// );
 // 	}
 // }
-import WordJC from "../../components/WordJC";
-import CreateForm from "../../components/CreateForm";
+  render() {
+    const decks = this.state;
+		return(
+			<div className = 'container'>
+				{this.state.decks.map(decks => (
+					<MiniCard 
+						word={decks.deckName}
+					/>
 
-import array from "../../array.json";
-
-class Home extends Component {
-
-	state = {
-		array
-	};
-
-	render() {
-		return (
-			<div className="container">
-				{this.state.array.map(array => (
-		          <MiniCard
-		            id={array.id}
-		            word={array.word}
-		          />
-		        ))}
+					))}
 			</div>
 		);
-	}
+  }
 }
+// import WordJC from "../../components/WordJC";
+// import CreateForm from "../../components/CreateForm";
 
-}
+// import array from "../../array.json";
+
+// class Home extends Component {
+
+// 	state = {
+// 		array
+// 	};
+
+// 	render() {
+// 		return (
+// 			<div className="container">
+// 				{this.state.array.map(array => (
+// 		          <MiniCard
+// 		            id={array.id}
+// 		            word={array.word}
+// 		          />
+// 		        ))}
+// 			</div>
+// 		);
+// 	}
+// }
 
 
 export default Home;
