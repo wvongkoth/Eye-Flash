20b995f1b9b131f8671e9da365b98dc933adf343
Will Vongkoth <avongkoth92@u.northwestern.edu>
2018-03-01 18:56:35 -0600

    commented out my code to be able to merge with caitlins for git purposes

--- my-react-app/src/pages/Home/Home.js
+++ my-react-app/src/pages/Home/Home.js
@@ -3,48 +3,45 @@ import JumboCard from "../../components/JumboCard";
 import Form from "../../components/Form";
 import MiniCard from "../../components/MiniCard";
 
-class Home extends Component{
-	constructor(props) {
-		super(props);
-			this.state = {
-				error: null,
-				isLoaded: false,
-				decks: []
-			};
-	}
+// class Home extends Component{
+// 	constructor(props) {
+// 		super(props);
+// 			this.state = {
+// 				error: null,
+// 				isLoaded: false,
+// 				decks: []
+// 			};
+// 	}
 
-  	componentDidMount() {
-	    fetch("/api/getAllDecks")
-	    	.then(res => res.json())
-	    	.then(
-	        (result) => {
-		        this.setState({
-		            isLoaded: true,
-		            decks: result.decks
-				});
-	   		},
-	   		(error) => {
-	   			this.setState({
-	   				isLoaded: true,
-	   				error
-	   			});
-	   		}
-	   	)
-	}	
+//   	componentDidMount() {
+// 	    fetch("/api/getAllDecks")
+// 	    	.then(res => res.json())
+// 	    	.then(
+// 	        (result) => {
+// 		        this.setState({
+// 		            isLoaded: true,
+// 		            decks: result.decks
+// 				});
+// 	   		},
+// 	   		(error) => {
+// 	   			this.setState({
+// 	   				isLoaded: true,
+// 	   				error
+// 	   			});
+// 	   		}
+// 	   	)
+// 	}	
 
-	render(){
-		return(
-			<div>
-				{this.state.decks.map(deck => (
-					<MiniCard />
+// 	render(){
+// 		return(
+// 			<div className = 'container'>
+// 				{this.state.decks.map(deck => (
+// 					<MiniCard />
 
-					))}
-			</div>
-		)
-	}
-
-}
-
-
+// 					))}
+// 			</div>
+// 		)
+// 	}
+// }
 
 export default Home;
