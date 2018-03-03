52c80dda742dcdcdfbe5ed0d9c39ae9d97f05dbd
ccowen <caitlincowen2016@u.northwestern.edu>
2018-03-03 14:41:46 -0600

    flashcard super props

--- client/src/components/TableJC/TableJC.js
+++ client/src/components/TableJC/TableJC.js
@@ -1,44 +1,78 @@
-import React from "react";
+import React, {Component} from "react";
 import "./TableJC.css";
 
-const TableJC = props => (
-    <span>
-        <table className="threeByThreeIconTable">
-            <tr>
-                <td>
-                    <img src={props.image1}>
-		        </td>
-                <td>
-                    <img src={props.image2}>
-		        </td>
-                <td>
-                    <img src={props.image3}>
-		        </td>
-		    </tr>
-            <tr>
-                <td>
-                    <img src={props.image4}>
-		        </td>
-                <td>
-                    <img src={props.image5}>
-		        </td>
-                <td>
-                    <img src={props.image6}>
-		        </td>
-		    </tr>
-            <tr>
-                <td>
-                    <img src={props.image7}>
-		        </td>
-                <td>
-                    <img src={props.image8}>
-		        </td>
-                <td>
-                    <img src={props.image9}>
-		        </td>
-		    </tr>
-		</table>
-    </span>
-    );
+class TableJC extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            secondClick: false,
+            clickedSrc: ""
+        }
+    }
+
+    clickTable = e => {
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({ 
+            secondClick: true, 
+            clickedSrc: e.target.src
+            }, function () {
+                console.log(this.state.firstClick + "console");
+            },
+        );
+      };
+
+    render() {
+        const {secondClick} = this.state;
+        return (
+            <span>
+                { secondClick ? 
+                    <img src={this.state.clickedSrc} /> : 
+                        <span>
+                            <table className="threeByThreeIconTable">
+                                <tr>
+                                    <td>
+                                        <img src={this.props.image} onClick={this.clickTable}/>
+                                    </td>
+                                    <td>
+                                        <img src={this.props.image} />
+                                    </td>
+                                    <td>
+                                        <img src={this.props.image} />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src={this.props.image} />
+                                    </td>
+                                    <td>
+                                        <img src={this.props.image} />
+                                    </td>
+                                    <td>
+                                        <img src={this.props.image} />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src={this.props.image} />
+                                    </td>
+                                    <td>
+                                        <img src={this.props.image} />
+                                    </td>
+                                    <td>
+                                        <img src={this.props.image} />
+                                    </td>
+                                </tr>
+                            </table>
+                        </span>
+                }
+            </span>
+        )
+    }
+
+};
+
+
                         
 export default TableJC;
