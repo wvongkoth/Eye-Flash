92b1f0c0b759049c47fdeb994bf0d18cadf21e39
Ronak Shah <ronakshah2013@u.northwestern.edu>
2018-03-03 10:59:57 -0600

    Merged Caitlyn's Changes

--- client/src/pages/SingleCardView/SingleCardView.js
+++ client/src/pages/SingleCardView/SingleCardView.js
@@ -4,30 +4,12 @@ import array from "../../array.json";
 import JumboCard from "../../components/JumboCard";
 import WordJC from "../../components/WordJC";
 
-class SingleCardView extends Component{
-	// Setting this.state.array to the array json array
-	state = {
-		array
-	};
-
-	clickWord = id => {
-		// Filter this.state.array for array with an id not equal to the id being removed
-	    const array = this.state.array.filter(array => array.id !== id);
-	    // Set this.state.array equal to the new array array
-	    this.setState({ array });
-	};
+class SingleCardView extends Component {
 
 	render() {
-	    return (
+		return (
 			<div>
 				<JumboCard>
-						{this.state.array.map(array => (
-				          <WordJC
-				          	clickWord={this.clickWord}
-				            id={array.id}
-				            word={array.word}
-				          />
-				        ))}
 				</JumboCard>
 			</div>
 		);
