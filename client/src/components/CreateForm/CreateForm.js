966f0ce8a405aa5e590c3ceac090fde9caa43708
Will Vongkoth <avongkoth92@u.northwestern.edu>
2018-03-05 20:23:56 -0600

    form values

--- client/src/components/CreateForm/CreateForm.js
+++ client/src/components/CreateForm/CreateForm.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./CreateForm.css";
 
-const CreateForm = () =>
-  <div className="row">
-      <div className="col s12">
-        <div className="card">
-          <div className="card-content">
-            <div className="input-field">
-              <i className="material-icons prefix">g_translate</i>
-              <input id="icon_prefix" type="text" className="validate" />
-              <label for="icon_prefix">Enter phrase or short sentence to translate (limit 50 charcters)</label>
+class CreateForm extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(props)
+  this.onChange = this.onChange.bind(this);
+  }
+
+
+  onChange(event) {
+this.setState({
+      cardFront: event.target.value
+    });
+    console.log(event.target.value)
+    // console.log(this.props)
+    // console.log(event.target.id)
+    // const state = this.state
+  //       state[event.target.name] = event.target.value;
+  //       this.setState(state);
+  }  
+
+
+render(){
+  return(
+    <div className="row">
+        <div className="col s12">
+          <div className="card">
+            <div className="card-content">
+              <div className="input-field">
+                <i className="material-icons prefix">g_translate</i>
+                <input id="icon_prefix" type="text" className="validate"  onChange={this.onChange} />
+                <label for="icon_prefix">Enter phrase or short sentence to translate (limit 50 charcters)</label>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    )
+  }
+}
         
 export default CreateForm;
--- client/src/pages/CreatePage/CreatePage.js
+++ client/src/pages/CreatePage/CreatePage.js
@@ -2,19 +2,70 @@ import React, { Component } from "react";
 import array from "../../array.json";
 import "./CreatePage.css";
 
+
 import CreateForm from "../../components/CreateForm";
 
 class CreatePage extends Component{
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			deckName : '',
+			cardFront : ''
+		}
+
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onChange(event) {
+		console.log(event.target.value)
+		this.setState({
+			deckName: event.target.value,
+			cardFront: event.target.value
+		});
+		// const state = this.state
+  //       state[event.target.name] = event.target.value;
+  //       this.setState(state);
+	}
+
+
+
+	onSubmit(event) {
+		console.log(event.target)
+		alert('A name was submitted: ' + this.state.cardFront);
+		event.preventDefault();
+		// const { deckName, cardFront } = this.state
+
+		const newDeckData = {
+			'deckName' : this.state.deckName,
+			'cardFront' : this.state.cardFront
+		}
+		console.log(newDeckData);
+
+		fetch('http://localhost:5000/api/addNewDeck', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: newDeckData
+
+		}).then((res) => res.json())
+			.then((data) => console.log(data))
+			.catch((err) => console.log(err))
+	}	
 
 	render() {
+		const { deckName, cardFront } = this.state;
 	    return (
 		    <div>
 		      <div className="row">
 		        <div className="col s12 infoLanguageTop">
 		          <div className="card infoLanguageTop">
 		            <div className="card-content">
-		              <span className="card-title">Deck Name:</span>
+		              <span className="card-title">Deck Name: </span>
+		              <input placeholder="Insert Deck Language" id="deckName" type="text" onChange={this.onChange}/>
 		              <p>I am writing in 
 		              	<span className="chip initialLanguageChip">
 						    English
@@ -22,18 +73,18 @@ class CreatePage extends Component{
 		              . I would like to translate to 
 		              </p>
 		              <div className="translatedLanguageChipDiv">
-		              	<div className="chips chips-autocomplete"></div>
+		              	<div className="chips chips-autocomplete">Insert desired language here</div>
 		              </div>
 		            </div>
 		          </div>
 		        </div>
 		      </div>
 	      		<div className="container">
-					<CreateForm />
-					<CreateForm />
-					<CreateForm />
-					<CreateForm />
-					<CreateForm />
+					<CreateForm value={this.cardFront} onChange={this.onChange} />
+
+					<button onClick={this.onSubmit} class="btn waves-effect waves" type="submit" name="action">Submit
+						<i class="material-icons right">send</i>
+					</button>
 				</div>
 			</div>
 		);
