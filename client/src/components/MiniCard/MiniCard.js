cb1350a7937ea74bc3a0880984bba6d0007480cd
ccowen <caitlincowen2016@u.northwestern.edu>
2018-03-08 12:58:11 -0600

    color, mockup folder

--- client/src/components/MiniCard/MiniCard.js
+++ client/src/components/MiniCard/MiniCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./MiniCard.css";
 
 const MiniCard = props => (
-    <div className="miniCard">
+    <div className="miniCard" className={props.translatedLanguage}>
       <p className="centerTextMC" id={props.id}>{props.word}</p>
     </div>
 );
