84312d507b584142406d4409d1f7ee7d17d5fcf8
ccowen <caitlincowen2016@u.northwestern.edu>
2018-03-09 12:08:44 -0600

    minicard css

--- client/src/components/MiniCard/MiniCard.js
+++ client/src/components/MiniCard/MiniCard.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./MiniCard.css";
 
-
 const MiniCard = props => (
-    <div className={props.translatedLanguage} className="miniCard" onClick={(event)=>{props.clickHandler(props.deckID)}} data-deckID={props.deckID}>
+    <div className={props.translatedLanguage} className="miniCard z-depth-3" onClick={(event)=>{props.clickHandler(props.deckID)}} data-deckID={props.deckID}>
+        <svg className="cornerTabMC" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 59.83 61.01">
+		    <title>cornerTabMC</title>
+		    <g id="cornerTabMC" data-name="Layer 1">
+		          <polygon class="cornerTabMC" points="59.83 61.01 59.83 0 1.18 0 0 1.18 59.83 61.01" />
+		    </g>
+		</svg>
+	  <p className="translatedLanguageInitials">{props.initials}</p>
       <p className="centerTextMC">{props.word}</p>
     </div>
 );
